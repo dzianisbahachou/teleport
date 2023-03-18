f37699ed02305525b86711ad7948cb40c3409fba
dzianisbahachou <dzianis.bahachou@gmail.com>
2023-03-18 16:10:16 +0300

    Header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,45 +1,43 @@
 import { NavLink } from "react-router-dom";
 import classes from "./Header.module.css";
 import logo from "../assets/React-icon.png";
+import telegram from "../assets/telegram.png";
+import instagram from "../assets/instagram.png";
+import vk from "../assets/vk.png";
 
 const Header = () => {
     return (
       <header className={classes.header}>
-        <nav>
+        <nav className={classes.navbar}>
           <div className={classes.logo}>
-            <NavLink to="/" className={(({isActive}) => isActive ? classes.active : "")} end>
-              <img src={logo} alt="Лого"/>
+            <NavLink to="/" style={{background: "red", paddingTop: "10px"}} className={(({isActive}) => isActive ? classes.active : "")} end>
+              <img src={logo} className={classes.qq} alt="Лого"/>
             </NavLink>
           </div>
           <ul className={classes.list}>
             <li>
-              <NavLink to="/animators" className={(({isActive}) => isActive ? classes.active : "")}>Events</NavLink>
+              <NavLink to="animators" className={(({isActive}) => isActive ? classes.active : "")}><p className={classes["header-items"]}>Аниматоры</p></NavLink>
             </li>
             <li>
-              <NavLink to="animators" className={(({isActive}) => isActive ? classes.active : "")}>Newsletter</NavLink>
+              <NavLink to="animators" className={(({isActive}) => isActive ? classes.active : "")}><p className={classes["header-items"]}>Шоу-программы</p></NavLink>
             </li>
             <li>
-              <NavLink to="animators" className={(({isActive}) => isActive ? classes.active : "")}>Newsletter</NavLink>
+              <NavLink to="animators" className={(({isActive}) => isActive ? classes.active : "")}><p className={classes["header-items"]}>Доп услуги</p></NavLink>
             </li>
             <li>
-              <NavLink to="animators" className={(({isActive}) => isActive ? classes.active : "")}>Newsletter</NavLink>
+              <NavLink to="animators" className={(({isActive}) => isActive ? classes.active : "")}><p className={classes["header-items"]}>Онлайн-праздники</p></NavLink>
+            </li>
+            <li>
+              <NavLink to="animators" className={(({isActive}) => isActive ? classes.active : "")}><p className={classes["header-items"]}>Отзывы</p></NavLink>
             </li>
           </ul>
-          <ul className={`${classes.list} ${classes.aa}`}>
-            <li>
-              +375 44 511 11 11
-            </li>
-            <li>
-              Инст
-            </li>
-            <li>
-              Вк
-            </li>
-            <li>
-              Телег
-            </li>
+          <ul className={`${classes.list} ${classes.contacts}`}>
+            <li><a href='tel:375445134151'>+375 44 513 41 51</a></li>
+            <li><a href="https://t.me/qwenpis" rel='noreferrer' target="_blank"><img src={telegram} alt="Telegram" width="20px"/></a></li>
+            <li><a href="https://www.instagram.com/direct/t/340282366841710300949128137501689590966" rel='noreferrer' target="_blank"><img src={instagram} alt="Inst" width="20px"/></a></li>
+            <li><a href="https://vk.com/im?media=&sel=-211042858" rel='noreferrer' target="_blank"><img src={vk} alt="VK" width="20px"/></a></li>
           </ul>
-        </nav>
+          </nav>
       </header>
     )
 }
