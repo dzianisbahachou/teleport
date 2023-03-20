5d3abc0afc214a8cba3c9b996a22ee452bb382e6
dzianisbahachou <dzianis.bahachou@gmail.com>
2023-03-20 09:38:50 +0300

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,11 @@ import RootLayoutPage from './pages/RootLayoutPage';
 import AuthPage, {action as authAction} from './pages/AuthPage';
 import ErrorPage from './pages/ErrorPage';
 import AdminPage, {loader as adminLoader} from './pages/AdminPage';
+import HomePage, {action as addUserAction} from "./pages/HomePage";
 
 const router = createBrowserRouter([
   {path: '/', element: <RootLayoutPage />, errorElement: <ErrorPage />, children: [
-    {path: '/', element: <HomePage/>, action: action},
+    {path: '/', element: <HomePage/>, action: addUserAction},
     {path: 'animators', element: <AnimatorsPage/>},
   ]},
   {path: '/auth', element: <AuthPage />, errorElement: <ErrorPage />, action: authAction},
