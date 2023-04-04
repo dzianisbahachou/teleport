c693ef59d999fff15c65a14cfdb9b47ae2e3d7e1
Siarhei Shnitko <siarhei.shnitko@leverx.com>
2023-04-04 13:19:15 +0300

    fix main banner

--- src/components/MainBanner/MainBanner.jsx
+++ src/components/MainBanner/MainBanner.jsx
@@ -16,16 +16,18 @@ const MainBanner = () => {
         {/* <Modal show={modalIsOpened} closed={closeModal}/>
         <Backdrop show={this.state.modalIsOpen} /> */}
         <div className={classes.welcome}>
-            {/* <img src="/icons/banner.webp" alt="Banner" className={classes.banners}/> */}
-            <Container>
-                <h1 className={classes.title}>СТУДИЯ ДЕТСКИХ ПРАЗДНИКОВ В БРЕСТЕ</h1>
-                <p className={classes.description}>
-                Детские праздники в Бресте и Брестской области.<br/>
-                Наши герои создадут веселье где угодно: в квартире, <br/>загородном доме, кафе, школе или детский саду.<br/> 
-                Забронировать волшебный праздник с нашей студией легко.
-                </p>
-                <button className={classes["main-button"]}><a href="#magicForm">Вызвать волшебников</a></button>
-            </Container>
+            <img src="/assets/main.png" alt="Banner" className={classes.banner}/>
+            <div className={classes.info}>
+                <Container>
+                    <h1 className={classes.title}>СТУДИЯ ДЕТСКИХ ПРАЗДНИКОВ В БРЕСТЕ</h1>
+                    <p className={classes.description}>
+                    Детские праздники в Бресте и Брестской области.<br/>
+                    Наши герои создадут веселье где угодно: в квартире, <br/>загородном доме, кафе, школе или детский саду.<br/> 
+                    Забронировать волшебный праздник с нашей студией легко.
+                    </p>
+                    <button className={classes["main-button"]}><a href="#magicForm">Вызвать волшебников</a></button>
+                </Container>
+            </div>
         </div>
     </>);
 };
