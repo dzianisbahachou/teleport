bf7fa2546d8cbd16a7e26f679ba316c4841c0c7e
dzianisbahachou <dzianis.bahachou@gmail.com>
2023-04-04 23:43:28 +0300

    Footer

--- src/pages/RootLayoutPage.jsx
+++ src/pages/RootLayoutPage.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from 'react-router-dom'
+import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
 
 export default function RootLayoutPage() {
@@ -6,8 +7,8 @@ export default function RootLayoutPage() {
     return (
         <>
             <Header/>
-            <Outlet />
-            {/* footer */}
+            <Outlet/>
+            <Footer/>
         </>
     );
 }
