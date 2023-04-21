c019e995c0b4cd6544c73eaf4060c30bcbba7e93
Siarhei Shnitko <siarhei.shnitko@leverx.com>
2023-04-21 17:47:53 +0300

    add wave

---
+++ src/components/UI/Wave/Wave.jsx
@@ -0,0 +1,12 @@
+import cl from './Wave.module.css'
+
+const Wave = () => {
+
+    return (
+        <div className={cl.wrapper}>
+            <div className={cl.wave}></div>
+        </div>
+    );
+};
+
+export default Wave;
