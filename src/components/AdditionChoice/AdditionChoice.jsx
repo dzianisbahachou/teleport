b6e45f852ec15ff154716f792bda6fd186ffa603
dzianisbahachou <dzianis.bahachou@gmail.com>
2023-04-21 16:31:02 +0300

    OP

--- src/components/AdditionChoice/AdditionChoice.jsx
+++ src/components/AdditionChoice/AdditionChoice.jsx
@@ -9,11 +9,35 @@ const AdditionChoice = () => {
         id:"1",
         price:123,
         title:"Скай"
+    },
+    {
+        description:"какое-то описание Скай",
+        eventSubType:"clown",
+        eventType:"animators",
+        id:"1",
+        price:123,
+        title:"Clown"
+    },
+    {
+        description:"какое-то описание Скай",
+        eventSubType:"mickeyMouse",
+        eventType:"animators",
+        id:"1",
+        price:123,
+        title:"mini mouse"
+    },
+    {
+        description:"какое-то описание Скай",
+        eventSubType:"mickeyMouse",
+        eventType:"animators",
+        id:"1",
+        price:123,
+        title:"mini mouse"
     }]
     return (
         <div className={cl.wrapper}>
             <ul className={cl.list}>
-                { data.map(event => <li key={event.id}><EventItem data={event}/></li>) }
+                { data.map(event => <li key={event.id}><EventItem width="15vw" data={event}/></li>) }
             </ul>
     </div>);
 };
--- src/components/EventItem/EventItem.jsx
+++ src/components/EventItem/EventItem.jsx
@@ -2,20 +2,18 @@ import { Link } from 'react-router-dom';
 import cl from './EventItem.module.css';
 import MainButton from '../UI/MainButton/MainButton';
 
-export default function EventItem({data}) {
+export default function EventItem({data, width}) {
     const eventSubType = data.eventSubType;
-    const imgPath = eventSubType 
-        ? `assets/logo/eventLogo/${eventSubType}.webp`
-        : 'assets/logo/defaultEventLogo.webp'; 
-
-        debugger
+    const imgPath = eventSubType  
+        ? `${process.env.PUBLIC_URL}/assets/logo/eventLogo/${eventSubType}.webp`
+        : `${process.env.PUBLIC_URL}/assets/logo/defaultEventLogo.webp`;
 
     return (
         <Link to={eventSubType} className={cl.item}>
             <div className={cl.action}>
                 <MainButton className={cl.button}>Подробнее</MainButton>
             </div>
-            <img className={cl.img} src={imgPath} alt={eventSubType}/>
+            <img style={{width: width}} className={cl.img} src={imgPath} alt={eventSubType}/>
             <span>{data.title}</span>
         </Link>
     );
